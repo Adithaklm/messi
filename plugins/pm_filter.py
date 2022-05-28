4850a552b06ffe421cb4ed997e4c1c83870ea28e
Adithak_99 <94104632+Adithaklm@users.noreply.github.com>
2022-05-28 13:18:12 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -705,8 +705,7 @@ async def auto_filter(client, msg, spoll=False):
     TEMPLATE = settings['template']
     if imdb:
         cap = TEMPLATE.format(
-            query=search,
-            message.from_user.mention=mention, 
+            query=search, 
             title=imdb['title'],
             votes=imdb['votes'],
             aka=imdb["aka"],
